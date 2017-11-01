8103f0bdf2cd235a79d30ece3e8610fa55c2fd36
yexiaoxiaogo <841079936@qq.com>
2017-11-01 21:25:28 +0800

    collection-hashset1-3

---
+++ src/io/github/yexiaoxiaogo/collection/hashset/HashSet1.java
@@ -0,0 +1,23 @@
+package io.github.yexiaoxiaogo.collection.hashset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSet1 {
+	public static void main(String[] args) {
+		HashSet<String> hashset = new HashSet<>();
+		hashset.add("a");
+		hashset.add("b");
+		hashset.add("c");
+		hashset.add("d");
+		hashset.add("e");
+		System.out.println(hashset);
+		// 2 Iterator set
+		Iterator<String> p = hashset.iterator();
+		while (p.hasNext()) {
+			System.out.println(p.next());
+		}
+		// 3 hashset size
+		System.out.println(hashset.size());
+	}
+}
