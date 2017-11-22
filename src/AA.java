b60e81e456bc5d62da362c22718a12bc34746647
yexiaoxiaogo <841079936@qq.com>
2017-11-23 01:49:55 +0800

    测试

--- src/AA.java
+++ src/AA.java
@@ -1,12 +1,10 @@
 
 public class AA {
 	public static void main(String[] args) {
-		int totalTime = 500;
-		int runTime = 60;
-		int restTime = 45;
-		int speed = 14;
-		int sum = 0;
-		
+		String str1 = "hello";
+		String str2 = "he" + new String("llo");
+		System.err.println(str1 == str2);
+
 	}
 
 }
