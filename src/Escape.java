ef72c342fd9e92616c51c9c0e21e39afed7f06ba
yexiaoxiaogo <841079936@qq.com>
2017-12-19 17:16:09 +0800

    转义

---
+++ src/Escape.java
@@ -0,0 +1,13 @@
+
+public class Escape {
+	public static void main(String[] args) {
+		
+		
+		String code="123";
+		
+		String source="  {\"key\":\" " +  code  + "  \"}    ";
+		
+		System.out.println(source);
+	}
+
+}
